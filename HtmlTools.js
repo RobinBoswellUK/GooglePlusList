cce3f59e3e50f17dfd8aaf90cbaa27a60e79eae7
doctorBobApplications <doctorbob567@gmail.com>
2015-10-22 09:57:29 +0100

    added more demos, fixed strings looking like arrays created test

--- HtmlTools.js
+++ HtmlTools.js
@@ -2,7 +2,7 @@
  *  Tools
  *
  */
-
+log("including HtmlTools.js")
 
 /**
  * Function for html building, wraps any string
@@ -22,25 +22,32 @@ function HtmlBuilder_wrap(s, prefix, suffix){
  * @param   element   string specifying element 
  */
 function HtmlBuilder_element(s, element){
-    return  "<"+element+">"+s+"</"+element+">"
+    return  "<"+ element +">"+ s +"</"+ element +">"
 }
 
 /**
  * Function for html building, wraps each element in an array
  *
- * @param   a       array to be wrapped
- * @param   prefix  prefix
- * @param   suffix  suffix
- */function HtmlBuilder_wrapArray(a, prefix, suffix){
+ * @param   inarray  array to be wrapped
+ * @param   prefix   prefix
+ * @param   suffix   suffix
+ */function HtmlBuilder_wrapArray(inarray, prefix, suffix){
     s = "";
-    for (var i=0; i<a.length; i++){
-        s += prefix + a[i] + suffix
+    //check it is not a string, masqurading as an array
+    if("string"==(typeof inarray)) {
+		log("found string")
+        s = prefix + inarray + suffix
+    }else{  
+      for (var i=0; i<inarray.length; i++){
+          s += prefix + inarray[i] + suffix
+      }
     }
     return s
 }
 
 
 /**
+ *  @class TableGrid
  * <b>Class</b>
  * <p>tool for building tables and grids
  * <p>These types of tasks involve building one line at a time, and can be
@@ -55,13 +62,15 @@ function HtmlBuilder_element(s, element){
  *
  */
 function TableGrid(){
+
     this.linebuilder
     this.linePrefix
     this.lineSuffix
     this.block=""
     
     /**
-     * function <b>setLineBuilder</b> to set the line builder function and line beginning and end string
+     * @memberof TableGrid
+     * <hr>function <b>setLineBuilder</b> <hr> to set the line builder function and line beginning and end string
      * @param   lb  linebuilder function
      * @param   pre prefix
      * @param   suf suffix
@@ -75,19 +84,19 @@ function TableGrid(){
      }
      
     /**
-     *    function <b>buildRows</b> to do the line build
-     *    will loop over the top layer and pass each item to line builder
+     *    <hr>function <b>buildRows</b><hr> to do the line build
+     *    will loop over the top layer and pass item sto line builder
      *  @param  table   data to be wrapped
      */
     this.buildRows=function(table){
-        //will loop over the top layer and pass each item to line builder
+        //will loop over the top layer and pass item sto line builder
         for(var i=0; i<table.length; i++){
             tg.buildLine(table[i])
         }
     }
     
     /**
-     * function <b>buildLine</b> to generate the line, wraps with the line beggining and end
+     * <hr>function <b>buildLine</b><hr> to generate the line, wraps with the line beggining and end
      *  @param  table   data to be wrapped
      */
     this.buildLine=function(data){
@@ -104,9 +113,78 @@ function TableGrid(){
     }
     
     /**
-     * function <b>getBlock</b>
+     * @memberof  TableGrid
+     * <hr>function <b>getBlock</b><hr>
      * @return the block
      */
-    this.getBlock=function(){ return this.block; }
+    this.getBlock=function(){
+        return this.block; }
     
+    
+    /**
+     *   Standard types
+     *
+     *
+     *
+     *
+     *
+     */
+
 }  //oef-class
+
+/**
+ *@memberof TableGrid
+ *  Static variables
+ */
+TableGrid.SIMPLE_TABLE_ROWS = 1;
+/**
+ *@memberof TableGrid
+ *  Static variables
+ */
+TableGrid.SIMPLE_TABLE_HEADER_ROW = 2;
+
+/**
+ *@memberof TableGrid
+ *  Static sample arrays
+ */
+TableGrid.LIST_ANIMALS = ['dog', "cat", "spider", "fly", "salmon", "shark", "frog", "newt", "eagle", "duck" ]
+
+/**
+ *  @class TableGrid
+ *  <hr>
+ *  function <b>createTableGrid</b> <hr>
+ *  Function to build predefined TableGrid instance 
+ *  @param id   id of type to generate
+ *      SIMPLE_TABLE_ROWS
+ *      SIMPLE_TABLE_HEADER_ROW
+ *  
+ */
+TableGrid.createTableGrid = function(id){
+    log("createTableGrid "+id)
+    switch (id){
+        // ********************************************
+       case TableGrid.SIMPLE_TABLE_ROWS: //TableGrid.SIMPLE_TABLE_ROWS:
+            myLineBuilder=function(data){
+                log("TableGrid.SIMPLE_TABLE_ROWS data isArray: "+ (data instanceof Array))
+                return HtmlBuilder_wrapArray(data,"<td>","</td>")
+            }
+            //log("create "+ myLineBuilder)
+            tg = new TableGrid()
+            tg.setLineBuilder( myLineBuilder, "\t<tr>", "</tr>\n")
+            return tg
+    
+        // ********************************************
+        case TableGrid.SIMPLE_TABLE_HEADER_ROW:
+            myLineBuilder=function(data){
+                log("TableGrid.SIMPLE_TABLE_HEADER_ROW data isArray: "+typeof data)
+                return HtmlBuilder_wrapArray(data,"<th>","</th>")
+            }
+            //log("create "+ myLineBuilder)
+            tg = new TableGrid()
+            tg.setLineBuilder( myLineBuilder, "\t<tr>", "</tr>\n")
+            return tg
+    }
+    log("createTableGrid no match ")
+    return false
+} //eo-func
+
--- codeBehind.js
+++ codeBehind.js
@@ -1,36 +1,76 @@
 window.onload = function(){
 
+
 // ********************************************
-// do simple example
-var htmlString=""
+// do simple test
+var htmlString="<hr>Test<br>"
 
-//add the header
-htmlString+="<table>\n"
+//TableGrid
+tg = TableGrid.createTableGrid(TableGrid.SIMPLE_TABLE_ROWS)
+tg.buildRows( TableGrid.LIST_ANIMALS )
 
+//get the lines block
+testString= tg.getBlock()
+
+//check the string
+testResult = "	<tr><td>dog</td></tr>\n\
+	<tr><td>cat</td>"==((testString).slice(0,40))
+//log(testString.slice(0,40))
+htmlString+= (testResult)? "PASS": "FAIL";
+
+$("result1").innerHTML = htmlString
+
+
+// ********************************************
+// do simple table example
+var htmlString="<hr>"
+
+//add the table header
+htmlString+="<table><caption>Simple Table</caption>\n"
+
+//start the table grid
 function myLineBuilder(ins){
     return HtmlBuilder_wrapArray(ins,"<td>","</td>")
 }
-
-//start the table grid
 tg = new TableGrid()
 tg.setLineBuilder(myLineBuilder, "\t<tr>", "</tr>\n")
+tg.buildLine( new Array(1,2,3) )
+tg.buildLine( new Array(4,5,6) )
 
-var lineData = new Array(1,2,3);
-tg.buildLine(lineData)
-
-tg.buildLine(new Array(4,5,6))
-
-//add the lines block
+//get the lines block
 htmlString+= tg.getBlock()
 
+//add table footer
+htmlString+="</table>"
+
+$("result2").innerHTML = htmlString
+
+
+// ********************************************
+// do a simple table using predefined
+var htmlString="<hr>"
+
+// add the header
+htmlString+="<table><caption>Simple Table Using Predefined</caption>\n"
+
+//start the table grid
+tg = TableGrid.createTableGrid(TableGrid.SIMPLE_TABLE_ROWS)
+tg.buildLine( new Array(7,8,9) )
+tg.buildLine( new Array(10,11,12) )
+
+//get the lines block
+htmlString+= tg.getBlock()
 //add footer
 htmlString+="</table>"
 
-//$("result").innerHTML = htmlString
+$("result3").innerHTML = htmlString
+
+
+
 
 // *******************************************
-// do a list
-htmlString = ''
+// do a list, using the g+ data
+htmlString = '<hr>'
 
 function myGridLineBuilder(data){
     s = '\n\t<div class="post_tags">\n\t\t'
@@ -45,12 +85,13 @@ function myGridLineBuilder(data){
 
 tg = new TableGrid()
 tg.setLineBuilder( myGridLineBuilder, '\n<div class="list_container">', '\n</div>')
+
 //add the lines block
 tg.buildRows(myPostList)
 htmlString += tg.getBlock()
 
 //$("result").textContent = htmlString
-$("result").innerHTML = htmlString
+$("result4").innerHTML = htmlString
 
 } //eof-onload
 
